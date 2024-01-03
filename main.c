de646910aec1ab9904deeb5fc38b0800b65559e1
Josiah Grimes <107405472+JoeyGrimes@users.noreply.github.com>
2024-01-03 02:30:24 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+LinkedList* list = init();
+
+
+
+    return 0;
+}
+
+
+
+//representative of each element within the actual list itself
+typedef struct Node{
+    // this is CURRENTLY NULL POINTER
+    struct Node *next; LL
+    int val;
+} Node;
+
+//The List itself
+typedef struct LinkedList {
+    // first node in the actual list
+    // this is CURRENTLY NULL POINTER
+    struct Node *firstNode;
+} LinkedList;
+
+
+//set the first node to 0 and return the list
+LinkedList* init(){
+//malloc the memory and make a pointer to the list
+// cast the malloc pointer to a LinkedList pointer
+// set the firstNode to be Null
+//return the listpointer
+    LinkedList* ptrList = (LinkedList*)malloc(sizeof(LinkedList));
+    ptrList->firstNode = NULL;
+    return ptrList;
+
+}
+
